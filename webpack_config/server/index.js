6ac3049a4f0d43297896723e2356c72a0b9f1db1
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-12 06:28:59 +0300

    fix(webpack_config): remove eslint errors in server config
    fix(webpack_config): remove eslint errors in server config

--- webpack_config/server/index.js
+++ webpack_config/server/index.js
@@ -5,26 +5,17 @@ import config from '../config'
 import isomorphicWebpackConfig from '../webpack.isomorphic'
 import childProcess from 'child_process'
 
-const {
-	SENTRY_DSN,
-	CLIENT_DIST_PATH,
-	JWT_SECRET,
-	ANALYZE_BUNDLE,
-	PORT,
-	isProduction
-} = config
-
 // Cleare dist dir before run
 const exec = childProcess.execSync
 exec(`rm -rf ${config.distPath}/server`)
 
 const definePluginArgs = {
 	'process.env.BROWSER': JSON.stringify(false),
-	'process.env.CLIENT_DIST_PATH': JSON.stringify(CLIENT_DIST_PATH)
+	'process.env.CLIENT_DIST_PATH': JSON.stringify(config.CLIENT_DIST_PATH)
 }
 
-const devtool = isProduction ? 'cheap-source-map' : 'source-map'
-const entry = isProduction
+const devtool = config.isProduction ? 'cheap-source-map' : 'source-map'
+const entry = config.isProduction
 	? path.join(config.srcPath, './server')
 	: path.join(config.srcPath, './server/decorator')
 
