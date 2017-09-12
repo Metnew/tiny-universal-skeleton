604dc9562a6749de18491869f7b404463ac0a6fa
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-12 06:30:07 +0300

    feat(server): add server decorator
    feat(server): add server decorator

---
+++ src/server/decorator.js
@@ -0,0 +1,21 @@
+/*
+ * @flow
+ */
+import addMiddlewares from './middlewares'
+import API from './api'
+import SSR from './ssr'
+
+/**
+ * Mount API, SSR and middlewares to app.
+ * @param  {Object} app - Express server instance
+ * @return {Object}     - Decorated server instance
+ */
+export default (app: Object): Object => {
+	// Add global middlewares
+	addMiddlewares(app)
+	// Add API
+	app.use('/api/v1', API)
+	// Add SSR
+	app.use(SSR)
+	return app
+}
