b18997e1d97cc5019b77c0a0e7b1ab945935be52
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-12 16:22:21 +0300

    fix(server): fix path to server decorator, fix import of server decorator
    fix(server): fix path to server decorator, fix import of server
    decorator

--- src/server/index.js
+++ src/server/index.js
@@ -5,10 +5,10 @@ const PORT = 3000
 const isProduction = process.env.NODE_ENV === 'production'
 const pathToServerDecorator = isProduction
 	? './decorator'
-	: '../../webpack_config/server'
+	: '../../webpack_config/devServer'
 // NOTE: Such dynamic imports is a bad practice!
 // It's used here to show that our `serverDecorator` is a dynamic thing.
-const serverDecorator = require(pathToServerDecorator)
+const serverDecorator = require(pathToServerDecorator).default
 serverDecorator(app)
 
 app.listen(PORT, () => {
