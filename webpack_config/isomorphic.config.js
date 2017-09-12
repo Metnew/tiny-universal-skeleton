6cf0f940bb8e1fb4b2f6fe16a60c1749c8bd6baf
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-09-12 06:29:18 +0300

    feat(webpack_config): add shared webpack config
    feat(webpack_config): add shared webpack config

---
+++ webpack_config/isomorphic.config.js
@@ -0,0 +1,51 @@
+/**
+ * @file
+ */
+import path from 'path'
+import config from './config'
+import webpack from 'webpack'
+
+const {
+	srcPath,
+	rootPath,
+	srcCommonPath,
+	NODE_ENV,
+	isProduction
+} = config
+
+const definePluginArgs = {
+	'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+}
+
+export default {
+	resolve: {
+		extensions: ['.js', '.json', '.jsx'],
+		modules: [srcPath, path.join(rootPath, 'node_modules')]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				enforce: 'pre',
+				use: [
+					{
+						loader: 'eslint-loader',
+						options: {
+							fix: true
+						}
+					}
+				],
+				exclude: [/node_modules/]
+			},
+			{
+				test: /\.(js|jsx)$/,
+				use: 'babel-loader',
+				exclude: [/node_modules/]
+			}
+		]
+	},
+	plugins: [
+		new webpack.NamedModulesPlugin(),
+		new webpack.DefinePlugin(definePluginArgs)
+	]
+}
