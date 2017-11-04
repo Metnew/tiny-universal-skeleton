b440c71e4a54571ddc632bfbce7ccc369969d752
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-11-04 14:49:32 +0200

    fix(client): adapt to R16
    fix(client): adapt to R16

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,9 +1,9 @@
 // Application
 import React from 'react'
-import {render} from 'react-dom'
+import {hydrate} from 'react-dom'
 import Root from 'common'
 
-render(<Root />, document.getElementById('app'))
+hydrate(<Root />, document.getElementById('app'))
 
 if (module.hot) {
 	module.hot.accept()
