e14b24225bb9bf176f51646b59325f95ba8c722e
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-11-06 19:27:50 +0200

    fix(server): read stats file, instead of importing it
    fix(server): read stats file, instead of importing it.
    some users on Linux expected the case when there is no stats file
    found. That means server code is already running while stats aren’t
    written to dist dir.

--- src/server/ssr/index.js
+++ src/server/ssr/index.js
@@ -3,12 +3,10 @@ import {renderToString} from 'react-dom/server'
 import Html from './Html'
 import Root from 'common'
 import fs from 'fs'
-// const assets = require(`${process.env.CLIENT_DIST_PATH}/webpack-assets.json`)
-// const assets = fs.readFileSync(`${process.env.CLIENT_DIST_PATH}/webpack-assets.json`, 'utf8')
 
 export default (req, res, next) => {
 	const App = renderToString(<Root />)
-	const assets = fs.readFileSync(`${process.env.CLIENT_DIST_PATH}/webpack-assets.json`, 'utf8')
+	const assets = JSON.parse(fs.readFileSync(`webpack-assets`, 'utf8'))
 	const html = Html({App, assets: JSON.parse(assets)})
 	res.send(html)
 }
