9386e8242aa5d60ea46c98aa38644dd19f86268e
Vladimir Metnev <vladimirmetnew@gmail.com>
2017-11-06 18:49:32 +0200

    chore(server): use fs instead of import
    chore(server): use fs instead of import

--- src/server/ssr/index.js
+++ src/server/ssr/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import {renderToString} from 'react-dom/server'
-import assets from 'webpack-assets'
 import Html from './Html'
 import Root from 'common'
+import fs from 'fs'
+// const assets = require(`${process.env.CLIENT_DIST_PATH}/webpack-assets.json`)
+// const assets = fs.readFileSync(`${process.env.CLIENT_DIST_PATH}/webpack-assets.json`, 'utf8')
 
 export default (req, res, next) => {
 	const App = renderToString(<Root />)
-	const html = Html({App, assets})
+	const assets = fs.readFileSync(`${process.env.CLIENT_DIST_PATH}/webpack-assets.json`, 'utf8')
+	const html = Html({App, assets: JSON.parse(assets)})
 	res.send(html)
 }
